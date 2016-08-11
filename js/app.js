a68093b22ed68a6655b2dbed5dc5a4ab59e5f85e
Jake Carpenter <carpjaco@isu.edu>
2016-08-10 23:50:20 -0600

    Add reset for spinning pokeball

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 window.onload = function () {
+    // Get document elements
     var outTransfer = document.getElementById('outTransfer');
     var outEvolve = document.getElementById('outEvolve');
+
+    // Initialize calculator
     pidgyCalculator.init(outTransfer, outEvolve);
 
-    document.getElementById('btnCalc').addEventListener('click', function(event) {
-        event.preventDefault();
-        var pidgies = document.getElementById('txtPidgies').value;
-        var candies = document.getElementById('txtCandies').value;
-        pidgyCalculator.calculate(pidgies, candies, true); //temp force 'true'
+    // pokeball click event
+    document.getElementById('btnCalc')
+        .addEventListener('click', function(event) {
+            event.preventDefault();
+            var pidgies = document.getElementById('txtPidgies').value;
+            var candies = document.getElementById('txtCandies').value;
+            // Call calculator - forcing true for transfering pidgeotto
+            pidgyCalculator.calculate(pidgies, candies, true); 
 
-        this.className = "spin-image";
-    });
+            // Call timer to make the ball spin
+            setSpinTimer(this);
+        });
+}
+
+// Set CSS on image to spin and timer to turn CSS off
+function setSpinTimer(element) {
+    element.className = "spin-image";
+    setTimeout(function() {
+        element.className = "";    
+    }, 1000);
 }
